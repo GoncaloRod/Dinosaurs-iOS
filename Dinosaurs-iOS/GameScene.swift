e90e2be2894ede153ed71c3c9a12f05cd9ec9aae
Gonçalo Rodrigues <goncalorod@outlook.pt>
2022-05-13 15:59:26 +0100

    Game scene set up with transition

--- Dinosaurs-iOS/GameScene.swift
+++ Dinosaurs-iOS/GameScene.swift
@@ -1,24 +1,17 @@
 import SpriteKit
 import GameKit
 
-class MainMenuScene : SKScene {
+class GameScene : SKScene {
     
     override func didMove(to view: SKView) {
         
         backgroundColor = .white
         
-        let label = SKLabelNode(text: "Main Menu")
+        let label = SKLabelNode(text: "Game Scene")
         label.position = CGPoint(x: frame.midX, y: frame.midY)
         label.fontColor = .black
         
         addChild(label)
-        
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        let gameScene = GameScene(size: size)
-        view?.presentScene(gameScene)
         
     }
     
